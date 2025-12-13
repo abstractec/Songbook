d0d8a95ae6abbc923d1f50be0b4d5760390d4fe9
John Haselden <john.haselden@abstractec.co.uk>
2025-12-13 09:20:31 +0700

    Initial Commit

---
+++ SongbookTests/SongbookTests.swift
@@ -0,0 +1,17 @@
+//
+//  SongbookTests.swift
+//  SongbookTests
+//
+//  Created by John Haselden on 13/12/2025.
+//
+
+import Testing
+@testable import Songbook
+
+struct SongbookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
