ee0fc315ec2e5cef9ff0ce516ff500654b715c1d
John Haselden <john.haselden@abstractec.co.uk>
2026-01-05 10:22:12 +0700

    Export a bunch of chords and a set of chords you can import

--- SongbookTests/ChordExportImportTests.swift
+++ SongbookTests/ChordExportImportTests.swift
@@ -15,7 +15,7 @@ struct ChordExportImportTests {
         let aMinor = Chord(id: UUID(), rootNote: .A, chordType: .minor)
         
         let encoder = JSONEncoder()
-        encoder.outputFormatting = .prettyPrinted // Optional: for readable JSON
+        encoder.outputFormatting = .prettyPrinted
 
         do {
             let jsonData = try encoder.encode([aMinor])
@@ -69,5 +69,119 @@ struct ChordExportImportTests {
         }
 
     }
+    
+    /**
+     This function is here to export a bunch of minor chords so that you don't have to enter them all yourself
+     */
+    @Test func exportMajorChords() async throws {
+        var chords: [Chord] = []
+        
+        for note in allNotes {
+            chords.append(self.buildMajorChord(note.0, alteration: note.1))
+        }
 
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+
+        do {
+            let jsonData = try encoder.encode(chords)
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                print(jsonString)
+            }
+        } catch {
+            print("Failed to encode model: \(error.localizedDescription)")
+            #expect(Bool(false), "decoding error")
+        }
+    }
+    
+    @Test func exportMinorChords() async throws {
+        var chords: [Chord] = []
+        
+        for note in allNotes {
+            chords.append(self.buildMinorChord(note.0, alteration: note.1))
+        }
+
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+
+        do {
+            let jsonData = try encoder.encode(chords)
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                print(jsonString)
+            }
+        } catch {
+            print("Failed to encode model: \(error.localizedDescription)")
+            #expect(Bool(false), "decoding error")
+        }
+    }
+    
+    @Test func exportAllChords() async throws {
+        var chords: [Chord] = []
+        
+        for note in allNotes {
+            chords.append(self.buildMajorChord(note.0, alteration: note.1))
+            chords.append(self.buildMinorChord(note.0, alteration: note.1))
+            chords.append(self.buildMajorSeventhChord(note.0, alteration: note.1))
+            chords.append(self.buildMinorSeventhChord(note.0, alteration: note.1))
+            chords.append(self.buildDominantSeventhChord(note.0, alteration: note.1))
+            chords.append(self.buildHalfDiminisedSeventhChord(note.0, alteration: note.1))
+        }
+
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+
+        do {
+            let jsonData = try encoder.encode(chords)
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                print(jsonString)
+            }
+        } catch {
+            print("Failed to encode model: \(error.localizedDescription)")
+            #expect(Bool(false), "decoding error")
+        }
+
+    }
+    
+    private func buildMinorChord(_ rootNote: Note, alteration: Alteration = .natural) -> Chord {
+        return Chord(id: UUID(), rootNote: rootNote, rootNoteAlteration: alteration, chordType: .minor)
+    }
+
+    private func buildMajorChord(_ rootNote: Note, alteration: Alteration = .natural) -> Chord {
+        return Chord(id: UUID(), rootNote: rootNote, rootNoteAlteration: alteration)
+    }
+
+    private func buildMajorSeventhChord(_ rootNote: Note, alteration: Alteration = .natural) -> Chord {
+        return Chord(id: UUID(), rootNote: rootNote, rootNoteAlteration: alteration, chordType: .seventh, seventhType: .major)
+    }
+
+    private func buildMinorSeventhChord(_ rootNote: Note, alteration: Alteration = .natural) -> Chord {
+        return Chord(id: UUID(), rootNote: rootNote, rootNoteAlteration: alteration, chordType: .seventh, seventhType: .minor)
+    }
+
+    private func buildDominantSeventhChord(_ rootNote: Note, alteration: Alteration = .natural) -> Chord {
+        return Chord(id: UUID(), rootNote: rootNote, rootNoteAlteration: alteration, chordType: .seventh, seventhType: .dominant)
+    }
+
+    private func buildHalfDiminisedSeventhChord(_ rootNote: Note, alteration: Alteration = .natural) -> Chord {
+        return Chord(id: UUID(), rootNote: rootNote, rootNoteAlteration: alteration, chordType: .seventh, seventhType: .halfDimished)
+    }
+
+    private var allNotes: [(Note, Alteration)] {
+        let notes = [
+            (Note.A, Alteration.natural),
+            (Note.A, Alteration.sharp),
+            (Note.B, Alteration.natural),
+            (Note.C, Alteration.natural),
+            (Note.C, Alteration.sharp),
+            (Note.D, Alteration.natural),
+            (Note.D, Alteration.sharp),
+            (Note.E, Alteration.natural),
+            (Note.F, Alteration.natural),
+            (Note.F, Alteration.sharp),
+            (Note.G, Alteration.natural),
+            (Note.G, Alteration.sharp),
+        ]
+        
+        return notes
+    }
 }
