6aff2cb6a4d2c5daff365ce7f5fbfc8108f8658c
John Haselden <john.haselden@abstractec.co.uk>
2026-01-14 11:33:43 +0700

    Add in the song performance class

--- Songbook/Models/SongPerformance.swift
+++ Songbook/Models/SongPerformance.swift
@@ -1,17 +1,19 @@
 //
-//  Playlist.swift
+//  SongPerformance.swift
 //  Songbook
 //
-//  Created by John Haselden on 13/12/2025.
+//  Created by John Haselden on 14/01/2026.
 //
 
 import Foundation
 import SwiftData
 
 @Model
-public final class Playlist: Identifiable {
+public final class SongPerformance: Identifiable {
     @Attribute(.unique) public var id: UUID
     public var name: String
+    public var instrument: Instrument? = nil
+
     var songPerformances: [SongPerformance]
 
     init(id: UUID, name: String, songPerformances: [SongPerformance]) {
