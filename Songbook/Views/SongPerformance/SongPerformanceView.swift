7354cbe80a920a932010885a715d528e82d5180e
John Haselden <john.haselden@abstractec.co.uk>
2026-01-15 13:31:23 +0700

    Song performance stubs

---
+++ Songbook/Views/SongPerformance/SongPerformanceView.swift
@@ -0,0 +1,18 @@
+//
+//  SongPerformanceView.swift
+//  Songbook
+//
+//  Created by John Haselden on 15/01/2026.
+//
+
+import SwiftUI
+
+struct SongPerformanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongPerformanceView()
+}
---
+++ Songbook/Views/SongPerformance/SongPerformanceViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SongPerformanceViewModel.swift
+//  Songbook
+//
+//  Created by John Haselden on 15/01/2026.
+//
+
+import Foundation
+
+
+class SongPerformanceViewModel {
+    var songPerformance: SongPerformance
+}
